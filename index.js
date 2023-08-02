642f1ee4ac92792b6766ab5fac16249316e00594
Olutsha <smolutsha@gmail.com>
2023-08-02 11:43:43 +0200

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import * as sqlite from 'sqlite';
-import sqlite3 from 'sqlite3';
 import express from 'express';
+import {getGreetings,addGreeting} from './db.js'
+console.log('start')
+
 const app = express();
 const PORT = process.env.PORT || 3008;
 
@@ -11,64 +12,15 @@ res.json({
 greetings 
 })
 });
-
+app.post('/api/greetings', async (req,res) => {
+    const greeting = req.body.greeting;
+    const language = req.body.language;
+     await addGreeting(language, greeting)
+    res.json({
+        status : 'success',
+        message : `Added a greeting for ${language}`
+    })
+});
 
 app.listen(PORT, () => console.log(`app started on port ${PORT}`)
 );
-const db = await sqlite.open({
-    filename: './101.db',
-    driver: sqlite3.Database
-});
-
-
-
-
-
-async function getGreetings(){
-const result = await db.all(`select * from greetings`);
-return result;
-}
-const result = await getGreetings()
-console.log(result)
-
-async function addGreeting(language, greeting){
-    const sql = "insert into greetings(language,greeting) values(?,?)"
-   await db.run(sql, [language,greeting]);
-    }
-    async function deleteGreetingByLanguage(language){
-            }
-    async function deleteGreeting(id){
-        const sql = `delete from greetings where id = ?`;
-        await db.run(sql, [id]);
-    }
-
-
-    async function updateGreeting(language, greeting, id){
-        const sql = `update greetings set language =?, greeting = ? where id = ?`;
-        await db.run(sql, [language,greeting,id]);
-    }
-    const result1 = await getGreetings()
-    console.log(result1);
-    console.log('=====================')
-    await updateGreeting('Sepedi', 'Thobela',16)
-
-
-    console.log('=====================')
-
-    const result2 = await getGreetings()
-    console.log(result2);
-
-    
-//getGreetings().then(result=> console.log(result))
-//console.log(getGreetings)
-// db
-//.all('select *from greetings')
-//.then(result=> {
- //   console.log(result)
-//});
-const countResult = await db.get(`select count(*) as count from greetings`);
-console.log(countResult.count);
-
-console.log('end')
-
-
---
+++ migration/002-create-greetings-data.sql
@@ -0,0 +1,6 @@
+insert into greetings (language, greeting) values ('zulu', 'Sawubona');
+insert into greetings (language, greeting) values ('French', 'Bonjour');
+insert into greetings (language, greeting) values ('tswana', 'Dumelang');
+--add two more languages
+--delete from greetings where id > 1;
+--delete from greetings where id = 3;
